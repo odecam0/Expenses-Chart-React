6f1e8698f4e7dc1f207829c63730b9e8100db40d
Bruno Macedo <macedobruno@id.uff.br>
2022-07-11 19:33:48 -0300

    Balance component done

--- src/index.js
+++ src/index.js
@@ -4,13 +4,53 @@ import './index.css';
 // import App from './App';
 // import reportWebVitals from './reportWebVitals';
 
+// Loading the logo image as a React component
+import {ReactComponent as Logo} from './logo.svg';
+
+// Loading font used
+import WebFont from 'webfontloader';
+
+WebFont.load({
+    google: {
+	families: ['DM Sans']
+    }
+});
+
+
 class Balance extends React.Component {
+    constructor (props) {
+	super(props);
+	this.state = {
+	    balance: props.balance,
+	}
+    }
+    
+    outerdiv_style = {
+	backgroundColor: 'hsl(10, 79%, 65%)',
+	padding: '10px',
+	borderRadius: '15px',
+	marginBottom: '10px',
+	display: 'flex',
+	justifyContent: 'space-between',
+	width: '400px'
+    };
+
     render() {
 	return (
-	    <p>My balance</p>
+	    // <p>My balance</p>
+	    <div style={this.outerdiv_style}>
+		<div style={{position:'relative', left:'10px'}}>
+		    <p style={{color:'white', fontSize:'12px', weight:'400', position:'relative', top:'10px'}}>My balance</p>
+		    <p style={{color:'white', fontSize:'22px', fontWeight:'700'}}>{'$' + this.state.balance}</p>
+		</div>
+		<div style={{marginTop: 'auto', marginBottom: 'auto', position:'relative', right:'10px'}}>
+		    <Logo/>
+		</div>
+	    </div>
 	);
     } 
 };
+
 
 class Chart extends React.Component {
     render() {
@@ -29,18 +69,24 @@ class MonthTotal extends React.Component {
 };
 
 let main_div_style = {
-    display: 'flex',
-    justifyContent:'center',
-    // alignItems:'center',
+    position: 'absolute',
+    top:      '50%',
+    left:     '50%',
+    transform: 'translate(-50%, -50%)',
+    // backgroundColor: 'green'
+};
+
+let bot_div_style = {
+    backgroundColor: 'white',
+    padding: '5%',
+    borderRadius: '15px'
 };
 
 function App(props) {
     return (
 	<div style={main_div_style}>
-	    <div>
-		<Balance/>
-	    </div>
-	    <div>
+	    <Balance balance={1000}/>
+	    <div style={bot_div_style}>
 		<Chart/>
 		<MonthTotal/>
 	    </div>
@@ -49,7 +95,7 @@ function App(props) {
 };
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(<App/>)
+root.render(<App style={{backgroundColor: 'hsl(27, 66%, 92%)'}}/>)
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
